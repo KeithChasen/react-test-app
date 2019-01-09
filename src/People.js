2217e7810c5b371bb7ce43c18d1405a5837fb645
keithchasenm <keithchasen89@gmail.com>
2019-01-09 17:34:33 +0200

    add persons list const for listing

--- src/People.js
+++ src/People.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
 const People = ({people, deletePerson }) => {
+    const personsList = people.map(person => {
+        return (
+            <div className="person" key={person.id}>
+                <div>Name: {person.name}</div>
+                <div>Age: {person.age}</div>
+                <button onClick={() => {deletePerson(person.id)}}>Delete</button>
+            </div>
+        )
+    })
 
     return(
        <div className="person-list">
-           {
-               people.map(person => {
-                   return (
-                       <div className="person" key={person.id}>
-                           <div>Name: {person.name}</div>
-                           <div>Age: {person.age}</div>
-                           <button onClick={() => {deletePerson(person.id)}}>Delete</button>
-                       </div>
-                   )
-               })
-           }
+           { personsList }
        </div>
     )
 
