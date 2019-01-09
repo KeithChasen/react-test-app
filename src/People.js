57ff7c7c8e84452af35f4f9d5106b551da063a29
keithchasenm <keithchasen89@gmail.com>
2019-01-09 16:10:44 +0200

    get rid of extra const for people list

--- src/People.js
+++ src/People.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const People = ({people}) => {
 
-    const personList = people.map(person => {
-        return (
-            <div className="person" key={person.id}>
-                <div>Name: {person.name}</div>
-                <div>Age: {person.age}</div>
-            </div>
-        )
-    })
-
     return(
        <div className="person-list">
-           {personList}
+           {
+               people.map(person => {
+                   return (
+                       <div className="person" key={person.id}>
+                           <div>Name: {person.name}</div>
+                           <div>Age: {person.age}</div>
+                       </div>
+                   )
+               })
+           }
        </div>
     )
 
